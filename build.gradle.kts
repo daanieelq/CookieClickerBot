a5d689d17c291a20de8cd21d583ee305d924bf12
diDaniel :') <150684428+daanieelq@users.noreply.github.com>
2024-12-03 10:20:30 +0100

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jsoup:jsoup:1.16.1")
 }
 
 tasks.test {
